ead1e52283a84581ccfe636fb0e8951ad62acc75
Brandan Sayarath and Joshua Hellman <joshua@>
2020-01-29 13:50:47 -0800

    add player IDs to player array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,30 @@
 // //Business Logic
-function startGame(player1Name, player2Name) {
-  var player1 = player1Name;
-  player1.total = 0;
-  player2.name = player2Name;
-  player2.total = 0;
+function Game() {
+  this.players = [];
+  this.currentId = 0;
 }
+
+Game.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Game.prototype.addPlayer = function(player){
+  player.id = this.assignId();
+  this.players.push(player);
+}
+
+// function Player(name) {
+//   this.name = name;
+//   this.turnScore = 0
+//   this.totalScore = 0
+// }
+function Player(name, totalScore, turn) {
+  this.name = name;
+  this.totalScore = totalScore;
+  this.turn = turn;
+}
+
 function diceRoll(){
   var roll = Math.floor( Math.random() * 6 ) +1;
   console.log(roll)
@@ -14,31 +34,29 @@ function diceRoll(){
   return roll;
 }
 
-// Game.prototype.assignId = function() {
-//   this.currentId += 1
-//   return this.currentId
-// }
-
-// Game.prototype.addPlayer = function() {
-  
-// }
-
-
-
-
-
 
 //User Logic
+var game = new Game();
+
 $(document).ready(function(){
   $("#nameForm").submit(function(event){
     event.preventDefault();
     var player1Name = $("#Player1name").val();
     var player2Name = $("#Player2name").val();
+    var player1Score = 0;
+    var player2Score = 0;
+    var player1Turn = 0;
+    var player2Turn = 0;
     $("#player1namedisplay").append(player1Name);
     $("#player2namedisplay").append(player2Name);
     $("#nameForm").hide();
     $(".game").show();
-    startGame(player1Name, player2Name)
+    var player1 =  new Player(player1Name, player1Score, player1Turn)
+    var player2 =  new Player(player2Name, player2Score, player2Turn)
+    game.addPlayer(player1);
+    game.addPlayer(player2);
+
+    console.log(game.players)
   })
   $("#player1roll").click(function(){
     diceRoll();
@@ -50,3 +68,41 @@ $(document).ready(function(){
     
   })
 })
+
+
+
+//Graveyard ---------------------------------------------------
+
+
+// Player.prototype.addPlayer = function(player) {
+//   player.id = this.assignId();
+// }
+
+// Game.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
+
+
+// function Player(name){
+//   this.name = name;
+//   this.turnScore = 0;
+//   this.totalScore = 0;
+// }
+
+// function startGame(player1Name, player2Name) {
+//   var player1 = player1Name;
+//   player1.total = 0;
+//   player2.name = player2Name;
+//   player2.total = 0;
+// }
+
+
+// Game.prototype.assignId = function() {
+//   this.currentId += 1
+//   return this.currentId
+// }
+
+// Game.prototype.addPlayer = function() {
+  
+// }
